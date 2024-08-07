a9c6bf29e479453077c968411b0c46aafffb941b
Amar <amarhulamani14@gmail.com>
2024-08-07 17:23:03 +0530

    Second Commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ import joblib
 
 model = joblib.load('model.pkl')
 quality= ['Low','Medium','High']
+if 'results_df' not in st.session_state:
+    st.session_state.results_df = pd.DataFrame(columns=['Type','Air temperature','Process temperature','Rotational speed','Torque','Tool wear','Predicted_Class'])
 
 st.markdown("<h1 style='text-align: center;'>Predictive Maintenance Centre</h1>", unsafe_allow_html=True)
 
@@ -34,18 +36,33 @@ if st.button('Submit'):
         })
     
     prediction = model.predict(user_input)
+    new_result_df = pd.DataFrame({
+        'Type': [quality_type],
+        'Air temperature': [air_temp],
+        'Process temperature': [process_temp],
+        'Rotational speed': [speed],
+        'Torque': [torque],
+        'Tool wear':[tool],
+        'Predicted_Class':[prediction]
+        })
+
+    st.session_state.results_df = pd.concat([new_result_df, st.session_state.results_df], ignore_index=True)
+
     if prediction==0:
-        st.write('Normal Operation')
+        st.success('Normal Operation')
     elif prediction==1:
-        st.write('Tool Wear Failure')
+        st.error('Tool Wear Failure',icon="⚠️")
     elif prediction==2:
-        st.write('Heat Dissipation Failure')
+        st.error('Heat Dissipation Failure',icon="⚠️")
     elif prediction==3:
-        st.write('Power Failure')
+        st.error('Power Failure',icon="⚠️")
     elif prediction==4:
-        st.write('Overstrain Failure')
+        st.error('Overstrain Failure',icon="⚠️")
     elif prediction==5:
-        st.write('Other Failure (Random)')
+        st.warning('Other Failure (Random)',icon="⚠️")
+
+    st.write('Prediction Results:')
+    st.dataframe(st.session_state.results_df)
 
     
     
